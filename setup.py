93d40ea477deb2e1f29b139bb7482b239abd8ede
servancho <bxtgroup@gmail.com>
2015-10-06 20:50:59 +0300

    Updated setup

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='xddos',
-    version='1.0b1',
+    version='1.0b2',
     author='Dmitry Shilyaev',
     author_email='dima@justhost.ru',
     license='MIT',
     url='https://github.com/servancho/xddos',
+
+    install_requires=['argparse'],
 
     packages=find_packages(exclude=['tests.*', 'ddos.egg-info']),
     scripts=['xddos.py'],
