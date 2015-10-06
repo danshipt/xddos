9654125c7307c0f42fff8640562b8ce32b5b5809
servancho <bxtgroup@gmail.com>
2015-10-06 20:55:48 +0300

    Updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xddos',
-    version='1.0b3',
+    version='1.0b4',
     author='Dmitry Shilyaev',
     author_email='dima@justhost.ru',
     license='MIT',
