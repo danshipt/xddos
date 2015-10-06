f4805c1a87b02942013e8aed721f3db2e87ff271
servancho <bxtgroup@gmail.com>
2015-10-06 20:40:45 +0300

    Updated package info

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='anti-ddos',
+    name='xddos',
     version='1.0b1',
     author='Dmitry Shilyaev',
     author_email='dima@justhost.ru',
     license='MIT',
-    url='https://github.com/servancho/pytin',
+    url='https://github.com/servancho/xddos',
 
     packages=find_packages(exclude=['tests.*', 'ddos.egg-info']),
     scripts=['http_protector.py'],
